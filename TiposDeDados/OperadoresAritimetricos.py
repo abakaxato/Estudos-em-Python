6e0b8f1c9f2a3f12cd800d7877e4d3bb8b483568
Abakaxato <cauakika@gmail.com>
2025-10-27 16:22:36 -0300

    adicionando o corpo do modulo de estudo de sinais aritmétricos

---
+++ TiposDeDados/OperadoresAritimetricos.py
@@ -0,0 +1,34 @@
+#os operadores aritmetricos são basicamente os mesmos
+
+adicao = 10 + 9
+print("adição",adicao)
+
+subtracao = 10 - 9
+print("subtração",subtracao)
+
+multiplicacao = 10 * 9
+print("multiplicação",multiplicacao)
+#multiplicacao entre flout e int
+
+multiplicacao2 = 10 * 0.7
+print("multiplicação com float",multiplicacao2)
+
+# o retorno de qualquer divisao sempre vai dar float
+divisao = 10 / 10
+print("divisão",divisao)
+
+#divisao entre flout e int
+divisao2 = 10 / 1.5
+print("divisão com float",divisao2)
+
+#divisao com duas barras corta as casas decimais, fica apenas o numero inteiro
+divisao3 = 10 // 1.5
+print("divisão com 2 barras",divisao3)
+
+#basicamente um numero elevado a outro
+exponeciacao = 2 ** 10 
+print("Exponeciação",exponeciacao)
+
+#Resto da divisao
+resto_da_divisao = 10 % 3
+print("Resto da divisão", resto_da_divisao)
