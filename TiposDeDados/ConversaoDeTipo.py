59ccaeb0f56041cd4c3efba347bbec90f8092886
Abakaxato <cauakika@gmail.com>
2025-10-27 11:30:35 -0300

    Finalizando a aula de conversão de tipos :white_check_mark:

--- TiposDeDados/ConversaoDeTipo.py
+++ TiposDeDados/ConversaoDeTipo.py
@@ -1,2 +1,9 @@
-#colocando o valor entre aspas ele vai ser reconhecido como String como é o caso '1', mas colocando o 'Int' ele converte para inteiro
-print(int("1"), type("1"))
+#colocando o valor entre aspas ele vai ser reconhecido como String como é o caso "1"
+print(("1"), type("1"))
+#mas colocando o 'Int' ele converte para inteiro
+print(int("1"), type(int("1")))
+#convertendo de float para int
+print(float(11))
+#convertendo String para boolean, vazio= False, <>vazio é igual a True
+print(bool(" "))
+
