b28e8bc6962a2eafd88ec82e7403ad4d8f796560
Abakaxato <cauakika@gmail.com>
2025-10-27 15:19:48 -0300

    Mudança de localização para melhor organização do estudo ✏️

---
+++ TiposDeDados/EstudoDeVariaveis.py
@@ -0,0 +1,13 @@
+# A principal diferenca da declaração de variavel em python para o JAVA e que podemos não dizemos o tipo
+# a outra diferença que existe é a ordem, java = "tipo variavel = valor", Python = "variavel = tipo (valor)"
+
+#Outra particuliariedade é o snake case no Python. java = nomeComposto, Python = nome_composto
+
+#declaracao de interiro
+idade = int (22);
+#declaracao de String
+Nome = "Cauã Ricardo";
+#Declaracao de float
+peso = 44.44
+#declaracao de boolean
+esta_vivo = True;
