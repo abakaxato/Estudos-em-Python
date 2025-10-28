ef8d0e1bd551f8d0d82aa0d696a4ffa1c5b03fe7
Abakaxato <cauakika@gmail.com>
2025-10-28 17:43:39 -0300

    Atualização do exercicio v0.7✨

--- Exercicios/ExercicioIMC.py
+++ Exercicios/ExercicioIMC.py
@@ -4,8 +4,9 @@ preco2 = 0.0; produto2=None;
 preco3 = 0.0; produto3=None;
 preco4 = 0.0; produto4=None;
 preco5 = 0.0; produto5=None;
-role = bool(); cr = str (0);
+role = bool(); entrada = str (0);
 soma = float(0); sys = str(1);
+compras = int(1); comprando = str();
 
 #Inicio do programa
 while(sys != str(0)):
@@ -13,15 +14,15 @@ while(sys != str(0)):
     print("Antes de começarmos, gostaria de saber se voce é cliente ou colaborador...\n");
     print("por favor, se voce é cliente digite '1' senão, se for colaborador, digite '2'\n");
 
-    while(cr != str(3) and cr != str(2) and cr != str(1)):
-        cr = input("\nDigite 1 para cliente, 2 para colaborador ou 3 para sair: ");
-        if (cr == str(1)):
+    while(entrada != str(3) and entrada != str(2) and entrada != str(1)):
+        entrada = input("\nDigite 1 para cliente, 2 para colaborador ou 3 para sair: ");
+        if (entrada == str(1)):
             role = True;
             print("\nLegal !!!\n Seja bem vindo a nossa loja");
-        elif (cr ==str(2)):
+        elif (entrada ==str(2)):
             role = False;
-            print("\nOpa !!!\n Seja bem vindo de volta colaborador");
-        elif (cr ==str(3)):
+            print("\n\n\n\nOpa !!!\n Seja bem vindo de volta colaborador");
+        elif (entrada ==str(3)):
             role = False;
             print("\nObrigado por visitar nossa loja, volte sempre !!!\n");
         else:
@@ -44,45 +45,59 @@ while(sys != str(0)):
             print("Esta interessado em comprar algum produto desses ?")
             print("Se sim, digite o numero do produto que deseja comprar, se não, digite 0 para sair\n");
             
-            while(soma != 0):
-                cat = int(input("Numero do produto desejado (1 a 5) ou 0 para sair: "))
+            while(compras != 0):
+                cat = int(input("\nNumero do produto desejado (1 a 5) ou 0 para sair: "))
                 if (cat == 1):
                     soma = soma + preco1;
+                    print("\nVocê adicionou", produto1, "ao carrinho.");
                     print("\nvalor atual de compra R$"+ str(soma) + " : ");
-                    input("quer fechar a compra ? (digite 's' para sim ou 'n' para não) : ");
-                    if (input == 's'):
+                    comprando = input("quer fechar a compra ? (digite 's' para sim ou 'n' para não) : ");
+                    if (comprando == 's'):
+                        compras = 0;
                         print("\nObrigado por visitar nossa loja, volte sempre !!!\n");
+                        print("\n voce fechou uma compra de no valor de R$"+ str(soma) + " : ");
                     else:
                         continue;
                 elif (cat == 2):
                     soma = soma + preco2;
+                    print("\nVocê adicionou", produto2, "ao carrinho.");
                     print("\nvalor atual de compra R$"+ str(soma) + " : ");
-                    input("quer fechar a compra ? (digite 's' para sim ou 'n' para não) : ");
-                    if (input == 's'):
+                    comprando = input("quer fechar a compra ? (digite 's' para sim ou 'n' para não) : ");
+                    if (comprando == 's'):
+                        compras = 0;
                         print("\nObrigado por visitar nossa loja, volte sempre !!!\n");
+                        print("\n voce fechou uma compra de no valor de R$"+ str(soma) + " : ");
                     else:
                         continue;
                 elif (cat == 3):
                     soma = soma + preco3;
+                    print("\nVocê adicionou", produto3, "ao carrinho.");
                     print("\nvalor atual de compra R$"+ str(soma) + " : ");
-                    input("quer fechar a compra ? (digite 's' para sim ou 'n' para não) : ");
-                    if (input == 's'):
+                    comprando = input("quer fechar a compra ? (digite 's' para sim ou 'n' para não) : ");
+                    if (comprando == 's'):
+                        compras = 0;
                         print("\nObrigado por visitar nossa loja, volte sempre !!!\n");
+                        print("\n voce fechou uma compra de no valor de R$"+ str(soma) + " : ");
                     else:
                         continue;
                 elif (cat == 4):
                     soma = soma + preco4;
+                    print("\nVocê adicionou", produto4, "ao carrinho.");
                     print("\nvalor atual de compra R$"+ str(soma) + " : ");
-                    input("quer fechar a compra ? (digite 's' para sim ou 'n' para não) : ");
-                    if (input == 's'):
+                    comprando = input("quer fechar a compra ? (digite 's' para sim ou 'n' para não) : ");
+                    if (comprando == 's'):
+                        compras = 0;
                         print("\nObrigado por visitar nossa loja, volte sempre !!!\n");
+                        print("\n voce fechou uma compra de no valor de R$"+ str(soma) + " : ");
                     else:
                         continue;
                 elif (cat == 5):
                     soma = soma + preco5;
+                    print("\nVocê adicionou", produto5, "ao carrinho.");
                     print("\nvalor atual de compra R$"+ str(soma) + " : ");
-                    input("quer fechar a compra ? (digite 's' para sim ou 'n' para não) : ");
-                    if (input == 's'):
+                    comprando = input("quer fechar a compra ? (digite 's' para sim ou 'n' para não) : ");
+                    if (comprando == 's'):
+                        compras = 0;
                         print("\nObrigado por visitar nossa loja, volte sempre !!!\n");
                     else:
                         continue;
@@ -94,25 +109,50 @@ while(sys != str(0)):
         print("\nAgora que sabemos que voce é colaborador, vamos cadastrar produtos\n");
         
         produto1 = input("Digite o nome do produto 1: ");
-        preco1 = float(input("Digite o preço do produto 1: R$"));
+        while True:
+            try:
+                preco1 = float(input("Digite o preço do produto 1: R$"));
+                break
+            except ValueError:
+                print("\nValor inválido, por favor digite um valor numérico para o preço\n");
         
         produto2 = input("Digite o nome do produto 2: ");
-        preco2 = float(input("Digite o preço do produto 2: R$"));
+        while True:
+            try:
+                preco2 = float(input("Digite o preço do produto 2: R$"));
+                break
+            except ValueError:
+                print("\nValor inválido, por favor digite um valor numérico para o preço\n");
         
         produto3 = input("Digite o nome do produto 3: ");
-        preco3 = float(input("Digite o preço do produto 3: R$"));
+        while True:
+            try:
+                preco3 = float(input("Digite o preço do produto 3: R$"));
+                break
+            except ValueError:
+                print("\nValor inválido, por favor digite um valor numérico para o preço\n");
         
         produto4 = input("Digite o nome do produto 4: ");
-        preco4 = float(input("Digite o preço do produto 4: R$"));
+        while True:
+            try:
+                preco4 = float(input("Digite o preço do produto 4: R$"));
+                break
+            except ValueError:
+                print("\nValor inválido, por favor digite um valor numérico para o preço\n");
         
         produto5 = input("Digite o nome do produto 5: ");
-        preco5 = float(input("Digite o preço do produto 5: R$"));
+        while (True):
+            try:
+                preco5 = float(input("Digite o preço do produto 5: R$"));
+                break
+            except ValueError:
+                print("\nValor inválido, por favor digite um valor numérico para o preço\n");
         
         print("\nProdutos cadastrados com sucesso !!!\n");
     sys = input("Deseja reiniciar o sistema ? (digite 's' para sim ou 'n' para não) : ");
     if (sys == 's'):
         sys = str(1);
-        cr = str(0);
+        entrada = str(0);
     elif(sys == 'n'):
         sys = str(0);
         print("\nSistema finalizado, obrigado por usar nosso sistema !!!\n");
