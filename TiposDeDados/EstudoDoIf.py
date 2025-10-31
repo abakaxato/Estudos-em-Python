e69017d14509755cca58e00d18c8e4e41ed763db
Abakaxato <cauakika@gmail.com>
2025-10-31 19:21:52 -0300

    Estudo Simples de como funciona IF em python🚀

---
+++ TiposDeDados/EstudoDoIf.py
@@ -0,0 +1,13 @@
+# Não tem muita diferença do IF em JAVA para o IF em Python
+# Basicamente temos o "if" que seria o "SE", o "else" que seria o "SENÃO" e o "else" que seria o "SENÃO SE"
+# a declaração dos IFs que serem escritos em letras minusculas
+# não é recomendado usar chaves para delimitar o bloco de execução do IF em Python, o correto é usar dois pontos (:) e indentação
+
+entrada = input("Digite 's' ou 'n' : ")
+
+if(entrada == "s"):
+    print("você digitou 's'")
+elif(entrada == "n"):
+    print("Você digitou 'n'")
+else:
+    print("você digitou algo diferente de 's' ou 'n' ")
