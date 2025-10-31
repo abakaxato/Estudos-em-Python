6a2a58d2dd483e02350751d0de9458e7d0a05ae8
Abakaxato <cauakika@gmail.com>
2025-10-31 18:27:41 -0300

    Estudo de Input v0.01

---
+++ TiposDeDados/EstudoDeInput.py
@@ -0,0 +1,18 @@
+# a função que usamos para receber dados do usuario é a função input()
+
+# ela sempre retorna uma String, então se quisermos outro tipo de dado, precisamos converter o valor
+# para o tipo desejado
+
+#exemplo de input recebendo uma String
+nome = input("Digite seu nome: ");
+print(f"Seu nome é {nome}");
+#exemplo de input recebendo um inteiro
+idade = int(input("Digite sua idade: "));
+print(f"Sua idade é {idade}");
+#exemplo de input recebendo um float
+peso = float(input("Digite seu peso: "));
+print(f"Seu peso é {peso}");
+#Exemplo de input recebendo um boolean
+# o boolean identifica que se o valor for 0, ele é False, se for 1 ou mais, ele é True
+esta_vivo = bool(int(input("Digite 1 se você está vivo ou 0 se está morto: ")));
+print(f"Você está vivo ? {esta_vivo}");
