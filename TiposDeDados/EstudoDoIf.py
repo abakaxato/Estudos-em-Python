331c8767b42c2d861028672f042f387261b8d5e7
Abakaxato <cauakika@gmail.com>
2025-10-31 19:47:11 -0300

    Atualização do estudo IF em Python

--- TiposDeDados/EstudoDoIf.py
+++ TiposDeDados/EstudoDoIf.py
@@ -2,6 +2,7 @@
 # Basicamente temos o "if" que seria o "SE", o "else" que seria o "SENÃO" e o "else" que seria o "SENÃO SE"
 # a declaração dos IFs que serem escritos em letras minusculas
 # não é recomendado usar chaves para delimitar o bloco de execução do IF em Python, o correto é usar dois pontos (:) e indentação
+# também não precisa do () mas eu gosto de delimitar as coisas o maximo possivel
 
 entrada = input("Digite 's' ou 'n' : ")
 
