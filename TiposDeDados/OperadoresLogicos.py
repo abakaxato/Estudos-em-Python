27b3b2f8cc8a5e657dc0b644b02d08e40379e647
Abakaxato <cauakika@gmail.com>
2025-11-03 10:13:15 -0300

    Adionando o modulo de estudo dos Operadores Logicos🚀

---
+++ TiposDeDados/OperadoresLogicos.py
@@ -0,0 +1,2 @@
+#Os operadores Logicos do Python são os mesmos das outras linguagens "and", "or" e "not"
+# também temos algums operadores especias como é o caso do "none" que serve para representar o vazio
