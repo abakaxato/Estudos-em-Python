83f95775859a67c70a1fa512b573ffbef252e7c9
Abakaxato <cauakika@gmail.com>
2025-11-07 17:20:43 -0300

    Estudo da função id() do python

--- TiposDeDados/TiposDeFuncoes.py
+++ TiposDeDados/TiposDeFuncoes.py
@@ -1,2 +1,6 @@
 #Esse modulo serve para mostrar/testar funções do python
 # se eu não já estudei a função, ela pode acabar em qualquer outro lugar desse projeto
+
+#Função para verificar a localização do dado na memoria (ID)
+var1 = "a"
+print (id(var1))
