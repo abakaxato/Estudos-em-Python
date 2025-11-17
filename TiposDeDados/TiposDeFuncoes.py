98e7f1c8ac8ed37ce62d7341d0d251e738eb5d6e
Abakaxato <cauakika@gmail.com>
2025-11-17 16:07:43 -0300

    Voltando os estudos ⚡️

--- TiposDeDados/TiposDeFuncoes.py
+++ TiposDeDados/TiposDeFuncoes.py
@@ -1,6 +1,11 @@
 #Esse modulo serve para mostrar/testar funções do python
 # se eu não já estudei a função, ela pode acabar em qualquer outro lugar desse projeto
 
+var1 = "abC"
+
 #Função para verificar a localização do dado na memoria (ID)
-var1 = "a"
 print (id(var1))
+#testando a função upper para deixar as letras maiúsculas
+print(var1.upper())
+#testando a função lower para deixar as letras minúsculas
+print(var1.lower())
