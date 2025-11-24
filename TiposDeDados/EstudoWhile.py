e33c187e90963829788706b9f0b97f0213e36206
Abakaxato <cauakika@gmail.com>
2025-11-24 17:06:45 -0300

    Atualização do estudo

--- TiposDeDados/EstudoWhile.py
+++ TiposDeDados/EstudoWhile.py
@@ -3,4 +3,15 @@
 contador = 0
 
 while contador < 100:
+    print(contador)
     contador +=1
+
+ #Reiniciando o Loop usando o continue
+    if contador == 40:
+        print(contador)
+        continue
+
+ #Quebrando o while usando o braak para parar o loop
+    if contador == 60:
+        print (f"{contador}")
+        break
