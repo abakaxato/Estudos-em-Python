398fb0e4bfcb81aa575ba174d63b7dcbadf04aef
Abakaxato <cauakika@gmail.com>
2026-07-28 21:04:52 -0300

    adicionando print do tipo do dado da resposta da api

--- APIs/TesetAPI.py
+++ APIs/TesetAPI.py
@@ -3,3 +3,4 @@ import requests as rq
 res = rq.get('https://cep.awesomeapi.com.br/json/50010-000')
 print('\n Resposta da api :\n' , res.json())
 print('\n Cidade :\n',res.json()['city'])
+print('\n tipo do dado do res\n',type(res))
